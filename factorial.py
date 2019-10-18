d28938f162716d89b2de3f5da153d5e7316a4228
Amansaini1709 <56715703+Amansaini1709@users.noreply.github.com>
2019-10-18 17:50:42 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,9 @@
+#factorial
+a=1
+n=int(input("enter the number"))
+for i in range(1,n+1):
+    j=a*i
+    a=j
+print(j)
+    
+ 
---
+++ odd series.py
@@ -0,0 +1,4 @@
+#python
+a=int(input("enter the number of terms"))
+for i in range(1,2*a,2):
+    print (i)
